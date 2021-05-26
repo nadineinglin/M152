b2a2ac52239661fc2cc411e05566f89dc6aa2a49
Nadine Inglin <Nadine_Inglin@sluz.ch>
2021-05-26 21:23:32 +0200

    Push for 27.5.21

---
+++ uebung7-2/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="content">Test</div>
+  {{myString}}
+  <input type="text" value="{{myString}}">
+  <ul>
+    <li v-for="todo in todos ">{{todo}}</li>
+  </ul>
+  <input v-model="newTodo">
+  <input v-model="" >
+  <button v-on:click="addTodo()">Add Something</button>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data: function () {
+      return {
+        myString: 'Hello World2',
+        todos: [
+          'Sachen kaufen',
+          'Sache passieret',
+          'Raoul',
+        ],
+        newTodo: ''
+      }},
+
+      methods: {
+        addTodo: function () {
+          this.todos.push({
+            name: this.newTodo,
+            time: parseInt(this.newTime)
+          });
+          this.newTodo = "";
+          this.newTime = 0;
+        }
+      },
+      computed:{
+      todoCount: function(){
+        return this.todos.length;
+      },
+      totalTime:function(){
+        var time =0;
+
+        this.todos.forEach(function (todo){
+          time += todo.time;
+        })
+        return  time;
+      }}}
+
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ uebung7-3/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+ <div>Ausgabe Noten</div>
+  <div v-for="fach in Noten">{{fach.name}}
+    <div v-for="note in fach.grades">{{note}}</div>
+  </div>
+  <div ></div>
+</template>
+
+<script>
+export default{
+  name:'App',
+  data: function (){
+    return{
+      Noten:[
+        {
+          name: "Mathematik",
+          grades: [5,4.7,3.5,4.1]
+        },
+        {
+          name: "Geschichte",
+          grades: [5.5,4.9,5.75]
+        },
+        {
+          name: "Physik",
+          grades: [2,2.7,4.3]
+        },
+        {
+          name: "Deutsch",
+          grades: [4.25,4.1,3.5]
+        },
+      ]
+    }
+    computed:{
+      average:function(){
+
+      }
+    }
+
+  }
+
+}
+
+
+</script>
+
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
